ba573d5c175ad8fabb2ea6cd37e6308a924075e0
Bhargav-9121 <bkc.edu9121@gmail.com>
2023-06-22 01:43:19 +0530

    commit

--- Project38/settings.py
+++ Project38/settings.py
@@ -121,3 +121,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['35.154.138.221', 'localhost', '0.0.0.0']
